94d922a45df31b25d79da6f112fe29da22ffbeb8
amr <amr@Amrs-MacBook-Air.local>
2021-04-25 16:11:07 -0700

    Add Devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
+    devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+    end
+    devise_scope :user do
+      get 'signup', to: 'devise/registrations#new'
+    end
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
