2d83fc108ff69ca448f4202a311ccb6da2e66344
amro <amr@Amrs-MacBook-Air.local>
2021-10-30 18:37:38 -0700

    Nuke RoR Project

--- app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-  def index
-    render html: 'Writing a7a.io....check again later'
-  end
-end
--- app/models/thought.rb
+++
@@ -1,6 +0,0 @@
-class Thought < ApplicationRecord
-  belongs_to :user
-  default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true
-  validates :head, presence: true, length: { maximum: 1000 }
-end
